8be60c9ea4cdfaef917565d7f65cf2a9d901ea59
miyata09x0084 <59190800+miyata09x0084@users.noreply.github.com>
2023-02-20 11:16:28 +0900

    Create index.jsx

---
+++ services/index.jsx
@@ -0,0 +1,124 @@
+import { request, gql } from 'graphql-request';
+
+const graphqlAPI = process.env.NEXT_PUBLIC_GRAPHCMS_ENDPOINT;
+
+export const getPosts = async () => {
+    const query = gql`
+        query MyQuery {
+            postsConnection {
+            edges {
+                node {
+                author {
+                    bio
+                    name
+                    id
+                    photo {
+                    url
+                    }
+                }
+                createdAt
+                slug
+                title
+                excerpt
+                featuredImage {
+                    url
+                }
+                categories {
+                    name
+                    slug
+                }
+                }
+            }
+            }
+        }
+    `
+
+    const result = await request(graphqlAPI, query);
+
+    return result.postsConnection.edges;
+}
+
+export const getPostDetails = async (slug) => {
+    const query = gql`
+        query GetPostDetails($slug: String!) {
+            post(where: { slug: $slug }) {
+                author {
+                    bio
+                    name
+                    id
+                    photo {
+                    url
+                    }
+                }
+                createdAt
+                slug
+                title
+                excerpt
+                featuredImage {
+                    url
+                }
+                categories {
+                    name
+                    slug
+                }
+                content {
+                    raw
+                }
+            }
+        }
+    `
+
+    const result = await request(graphqlAPI, query, { slug });
+
+    return result.post;
+}
+
+export const getCategories = async () => {
+    const query = gql`
+      query GetCategories {
+        categories {
+          name
+          slug
+        }
+      }
+    `
+    const result = await request(graphqlAPI, query)
+
+    return result.categories;
+}
+
+export const getCategoryPost = async (slug) => {
+    const query = gql`
+      query GetCategoryPost($slug: String!) {
+        postsConnection(where: {categories_some: {slug: $slug}}) {
+          edges {
+            cursor
+            node {
+              author {
+                bio
+                name
+                id
+                photo {
+                  url
+                }
+              }
+              createdAt
+              slug
+              title
+              excerpt
+              featuredImage {
+                url
+              }
+              categories {
+                name
+                slug
+              }
+            }
+          }
+        }
+      }
+    `;
+    const result = await request(graphqlAPI, query, { slug });
+    console.log(query)
+    return result.postsConnection.edges;
+};
