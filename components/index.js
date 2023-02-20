00a4a8c99be78450d90726600e4a0ac160aa94ac
miyata09x0084 <59190800+miyata09x0084@users.noreply.github.com>
2023-02-20 11:16:01 +0900

    Create index.js

---
+++ components/index.js
@@ -0,0 +1,6 @@
+export { default as PostCard } from './PostCard';
+export { default as Categories } from './Categories';
+export { default as Header } from './Header';
+export { default as Layout } from './Layout';
+export { default as PostDetail } from './PostDetail';
+export { default as Loader } from './Loader';
