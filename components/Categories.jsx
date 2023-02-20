b9af1f9e6372ba79186bb92f8e3b18ac02c48e96
miyata09x0084 <59190800+miyata09x0084@users.noreply.github.com>
2023-02-20 11:15:41 +0900

    Create Categories.jsx

---
+++ components/Categories.jsx
@@ -0,0 +1,25 @@
+import React, { useEffect, useState } from 'react';
+import Link from 'next/link';
+import { getCategories } from '../services';
+
+const Categories = () => {
+  const [categories, setCategories] = useState([]);
+
+  useEffect(() => {
+    getCategories()
+      .then((newCategories) => setCategories(newCategories))
+  }, [])
+  return (
+    <div>
+      {categories.map((category) => (
+        <Link key={category.slug} href={`/category/${category.slug}`}>
+          <span className='pr-5'>
+            {category.name}
+          </span>
+        </Link>
+      ))}
+    </div>
+  )
+}
+
+export default Categories
