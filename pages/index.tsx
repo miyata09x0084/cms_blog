f625daed8e67c0baaaaea239da09679248e5e55b
miyata09x0084 <59190800+miyata09x0084@users.noreply.github.com>
2023-02-20 11:16:25 +0900

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,29 +1,34 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
+import { PostCard, Categories, Header } from '../components';
+import { getPosts } from '../services';
+import { request, gql } from 'graphql-request';
 
-const posts = [
-  { title: 'React Testing', excerpt: 'Learn React Testing' },
-  { title: 'React with Tailwind', excerpt: 'Learn React with Tailwind' }
-]
-
-const Home: NextPage = () => {
+const Home: NextPage = ({ posts }) => {
   return (
-    <div className="container mx-auto px-10 mb-8 bg-gray-300">
+    <div className="container mx-auto px-10 mb-8">
       <Head>
         <title>CMS Blog</title>
         <link rel="icon" href="/favicon.ico" />
-      </Head> 
-      <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
-        {posts.map((post, index) => (
-          <div>
-            {post.title}
-            {post.excerpt}
-          </div>
-        ))}
+      </Head>
+      <div className=''>
+        <div className='text-center pt-12 pb-12 border-b border-t border-gray-300 mt-12 mb-12'>
+              <Categories />
+        </div>
+        <div className=''>
+          {posts.map((post) => (<PostCard post={post.node} key={post.title} />))}
+        </div>
       </div>
-
     </div>
   )
 }
 
-export default Home
+export async function getStaticProps() {
+  const posts = (await getPosts()) || [];
+
+  return {
+    props: { posts }
+  }
+}
+
+export default Home;
