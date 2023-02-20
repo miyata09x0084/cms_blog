e47f7e61f46221b545ba46b4d4852b305e3f1d4f
miyata09x0084 <59190800+miyata09x0084@users.noreply.github.com>
2023-02-20 11:16:08 +0900

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,14 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import React, { useEffect, useState } from 'react';
+import { Layout } from '../components';
+import type { AppProps } from 'next/app';
+import '../styles/global.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+  )
 }
 
 export default MyApp
