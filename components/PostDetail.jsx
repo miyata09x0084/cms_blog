ea2b5b447086c9474fc5e217fc6a4eecf0a2d94d
miyata09x0084 <59190800+miyata09x0084@users.noreply.github.com>
2023-02-23 13:24:08 +0900

    Allow color switching in dark mode.

--- components/PostDetail.jsx
+++ components/PostDetail.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import moment from 'moment';
 import Link from 'next/link';
+import { useTheme } from 'styled-components';
 
 const PostDetail = ({ post }) => {
+  const theme = useTheme()
 
   const getContentFragment = (index, text, obj, type) => {
     let modifiedText = text;
+
+    {console.log(index, type)}
 
     if (obj) {
       if (obj.bold) {
@@ -13,7 +17,7 @@ const PostDetail = ({ post }) => {
       }
 
       if (obj.italic) {
-        modifiedText = (<em key={index}>{text}</em>);
+        modifiedText = (<em style = {{color: theme.article}} className='flex justify-center mt-2 mb-16' key={index}>{text}</em>);
       }
 
       if (obj.underline) {
@@ -23,14 +27,14 @@ const PostDetail = ({ post }) => {
 
     switch (type) {
       case 'heading-three':
-        return <h3 key={index} className="text-xl font-semibold mb-4">{modifiedText.map((item, i) => <React.Fragment key={i}>{item}</React.Fragment>)}</h3>;
+        return <h3 key={index} style = {{color: theme.article}} className="text-xl mb-4 pb-2 border-b">{modifiedText.map((item, i) => <React.Fragment key={i}>{item}</React.Fragment>)}</h3>;
       case 'paragraph':
         return <p key={index} className="mb-8">{modifiedText.map((item, i) => <React.Fragment key={i}>{item}</React.Fragment>)}</p>;
       case 'heading-four':
-        return <h4 key={index} className="text-md font-semibold mb-4">{modifiedText.map((item, i) => <React.Fragment key={i}>{item}</React.Fragment>)}</h4>;
+        return <h4 key={index} style = {{color: theme.article}} className="text-md mb-4 pb-2 border-b">{modifiedText.map((item, i) => <React.Fragment key={i}>{item}</React.Fragment>)}</h4>;
       case 'image':
         return (
-          <div className='flex justify-center'>
+          <div className='flex justify-center mt-20'>
             <img
               alt={obj.title}
               height={obj.height}
@@ -46,12 +50,12 @@ const PostDetail = ({ post }) => {
 
   return (
     <div>
-        <div className='pt-12 pb-3'>
-          <h1 className='text-2xl pb-1 mb-1 border-b border-gray-300'>
+        <div className='pt-12 pb-3 xl:px-96'>
+          <h1 style = {{color: theme.article}} className='text-2xl pb-1 mb-1 border-b border-gray-300'>
               {post.title}
           </h1>
-          <div className='mb-12'>
-            <span>
+          <div className='mb-4'>
+            <span style = {{color: theme.article}}>
               {moment(post.createdAt).format('MMM DD YYYY')}
             </span>
           </div>
