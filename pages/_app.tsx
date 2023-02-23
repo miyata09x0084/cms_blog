addd888f18ef4b088d8c5ebbc5d1b70940f86475
miyata09x0084 <59190800+miyata09x0084@users.noreply.github.com>
2023-02-23 13:24:48 +0900

    Install a dark mode button.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,12 +2,29 @@ import React, { useEffect, useState } from 'react';
 import { Layout } from '../components';
 import type { AppProps } from 'next/app';
 import '../styles/global.css';
+import { ThemeProvider } from 'styled-components';
+import { StyledColor, lightTheme, darkTheme } from '../components/ColorTheme';
+import ToggleSwitch from '../components/ToggleSwitch';
+
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const [theme, setTheme] = useState(lightTheme);
+  const isDarkTheme = (theme === darkTheme);
+  const toggleTheme = () => {
+    setTheme(isDarkTheme ? lightTheme : darkTheme);
+  }
+
   return (
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
+    <ThemeProvider theme={isDarkTheme ? darkTheme : lightTheme} >
+      <StyledColor>
+      <div className='container mx-auto pt-2 lg:px-96'>
+        <ToggleSwitch toggleTheme={toggleTheme}/>
+    </div>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </StyledColor>
+    </ThemeProvider>
   )
 }
 
