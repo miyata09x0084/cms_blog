da6c07ebe2fb9ac886d24b49c5d0c8ed3a661a1d
miyata09x0084 <59190800+miyata09x0084@users.noreply.github.com>
2023-02-23 13:23:59 +0900

    Allow color switching in dark mode.

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,12 +1,14 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import Link from 'next/link';
+import { useTheme } from 'styled-components';
 
 const Header = () => {
+  const theme = useTheme()
   return (
     <div className='container mx-auto'>
         <Link href='/'>
-            <h1 className='text-center text-4xl mt-12'>
-                <span className='cursor-pointer font-bold text-4xl'>
+            <h1 className='text-center text-4xl mt-24'>
+                <span style = {{color: theme.site}} className='cursor-pointer font-bold text-4x'>
                     Rio's Website
                 </span>
             </h1>
