14cea24b423d937022a38fee5e2d5a7360c23661
miyata09x0084 <59190800+miyata09x0084@users.noreply.github.com>
2023-02-23 13:23:50 +0900

    Allow color switching in dark mode.

--- components/PostCard.jsx
+++ components/PostCard.jsx
@@ -1,17 +1,19 @@
 import React from 'react';
 import moment from 'moment';
 import Link from 'next/link';
+import { useTheme } from 'styled-components';
 
 const PostCard = ({ post }) => {
+  const theme = useTheme()
   return (
     <div className='pt-3 pb-3'>
       <div className='mb-0.5'>
-        <span>
+        <span style = {{color: theme.day}}>
           {moment(post.createdAt).format('MMM DD YYYY')}
         </span>
       </div>
       <h1 className='text-2xl text-myblue'>
-        <Link href={`/post/${post.slug}`}>
+        <Link style = {{color: theme.title}} href={`/post/${post.slug}`}>
           {post.title}
         </Link>
       </h1>
