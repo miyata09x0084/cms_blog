4a2c8efacb8983761c1bb7492498daa8929cb4e8
miyata09x0084 <59190800+miyata09x0084@users.noreply.github.com>
2023-02-23 13:25:47 +0900

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,25 +1,21 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
-import { PostCard, Categories, Header } from '../components';
+import { PostCard, Categories} from '../components';
 import { getPosts } from '../services';
-import { request, gql } from 'graphql-request';
 
 const Home: NextPage = ({ posts }) => {
+
   return (
-    <div className="container mx-auto px-10 mb-8">
-      <Head>
-        <title>CMS Blog</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <div className=''>
-        <div className='text-center pt-12 pb-12 border-b border-t border-gray-300 mt-12 mb-12'>
-              <Categories />
-        </div>
-        <div className=''>
-          {posts.map((post) => (<PostCard post={post.node} key={post.title} />))}
-        </div>
+      <div className="container mx-auto xl:px-96 mb-8">
+        <Head>
+          <title>CMS Blog</title>
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
+            <div className='text-center pt-8 pb-8 border-b border-t border-gray-300 mt-8 mb-12'>
+                  <Categories />
+            </div>
+            {posts.map((post) => (<PostCard post={post.node} key={post.title} />))}
       </div>
-    </div>
   )
 }
 
