72a91d086fb1f971260c9503a861b8b8eea661d7
miyata09x0084 <59190800+miyata09x0084@users.noreply.github.com>
2023-03-07 16:34:18 +0900

    Change name

--- components/Header.jsx
+++ components/Header.jsx
@@ -9,7 +9,7 @@ const Header = () => {
         <Link href='/'>
             <h1 className='text-center text-4xl mt-24'>
                 <span style = {{color: theme.site}} className='cursor-pointer font-bold text-4x'>
-                    Rio's Website
+                  Satoshi Nakamoto's Website
                 </span>
             </h1>
         </Link>
