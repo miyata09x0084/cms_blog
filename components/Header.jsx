a53df84b67988bc990b49d69ea45ec443e153bfc
miyata09x0084 <59190800+miyata09x0084@users.noreply.github.com>
2023-03-17 19:12:03 +0900

    Update Header.jsx

--- components/Header.jsx
+++ components/Header.jsx
@@ -5,15 +5,34 @@ import { useTheme } from 'styled-components';
 const Header = () => {
   const theme = useTheme()
   return (
-    <div className="mx-auto max-w-screen-md @screen px-8 md:px-0 mb-8 ">
-        <Link href='/'>
-            <h1 className='text-center text-4xl mt-24'>
-                <span style = {{color: theme.site}} className='cursor-pointer font-bold text-4x'>
-                  Rio Miyata's Website
-                </span>
-            </h1>
-        </Link>
-    </div>
+    <header>
+        <div className='container flex justify-between mx-auto max-w-screen-md @screen py-4 px-10 md:px-0 mb-8 font-MplusRounded text-xl font-mycolor'>
+          <div style = {{color: theme.site}} className='cursor-pointer items-center font-bold'>
+            <Link href='/'>
+              Rio Miyata
+            </Link>
+          </div>
+          <nav>
+            <ul className="flex space-x-4">
+              <li>
+                <Link href="/blog">
+                  Blog
+                </Link>
+              </li>
+              <li>
+                <Link href="/work">
+                  Work
+                </Link>
+              </li>
+              <li>
+                <Link href="https://github.com/miyata09x0084">
+                  Github
+                </Link>
+              </li>
+            </ul>
+          </nav>
+        </div>
+    </header>
   )
 }
 
