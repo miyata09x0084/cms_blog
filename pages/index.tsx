6fe6aeae62df9c1b41fb5002302492358ddf0c94
miyata09x0084 <59190800+miyata09x0084@users.noreply.github.com>
2023-03-18 20:46:47 +0900

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,8 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import { Box, Text, Image, Flex, Heading, VStack, HStack, Button } from '@chakra-ui/react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faStar } from '@fortawesome/free-solid-svg-icons';
 
 interface Props {
   posts: Array<any>;
@@ -9,7 +11,7 @@ interface Props {
 const Home: NextPage<Props> = () => {
 
   return (
-    <div className="mx-auto max-w-screen-md @screen px-8 md:px-0 mb-8 font-MplusRounded font-mycolor">
+    <div className="mx-auto max-w-screen-md @screen px-8 md:px-0 mb-8 font-MplusRounded font-mycolor tracking-tight">
       <Head>
         <title>Rio Miyata's Website</title>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
@@ -17,18 +19,19 @@ const Home: NextPage<Props> = () => {
         <link href="https://fonts.googleapis.com/css2?family=M+PLUS+Rounded+1c:wght@100;300;400;500;700;800;900&family=Source+Code+Pro:ital,wght@0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet" />
       </Head>
       {/* ChakraUi */}
-      <Box width="570px" mx="auto">
+      <Box width="570px" height="100vh" mx="auto">
         <Box boxSize='300px' mx="auto" marginBottom="30px" >
-          <Image src='/assets/images/computer-icon2.png' alt='computer' borderRadius="20px" opacity='0.9' />
+          <Image src='/assets/images/computer-icon2.png' alt='computer' borderRadius="20px" />
         </Box>
         <Box display="flex" justifyContent="center" flex="1">
-          <Text fontFamily="Source Code Pro" backgroundColor="white" borderRadius="20px" padding=" 20px 30px" fontWeight="bold">Welcome,I'm Web Developer/Blockchain Enthusiast👋</Text>
+          <Text fontFamily="Source Code Pro" backgroundColor="white" borderRadius="20px" padding=" 20px 30px" fontWeight="bold">Welcome, I'm Web Developer/Blockchain Enthusiast 👋</Text>
         </Box>
         <Flex width="100%" justifyContent="center" marginTop="20px" alignItems="center">
-          <Text fontSize="3xl" fontWeight="bold" marginRight="20px" marginLeft="15px">Rio Miyata</Text>
+          <Text fontSize="3xl" fontWeight="bold" marginRight="12px" marginLeft="15px">Rio Miyata</Text>
           <Image
             boxSize='70px'
             borderRadius='full'
+            border="1px solid #fff"
             src='https://avatars.githubusercontent.com/u/59190800?s=400&u=ea17e57c3dc9ef662b1f5ce525b8ebaf777e9713&v=4'
             alt='Rio Miyata'
           />
@@ -38,7 +41,7 @@ const Home: NextPage<Props> = () => {
             <Heading as="h2" size="md" fontFamily="M PLUS Rounded 1c">
               History
             </Heading>
-            <VStack align="start" spacing={1}>
+            <VStack align="start" spacing={2}>
               <Text>
                 2014 Master of Science in Engineering from Hosei University
               </Text>
@@ -54,20 +57,39 @@ const Home: NextPage<Props> = () => {
             </VStack>
           </VStack>
           <VStack align="start" spacing={2} marginTop="25px">
+            <Heading as="h2" size="md" fontFamily="M PLUS Rounded 1c">
+              <Flex>
+                  <Box marginRight="1px">
+                      My
+                  </Box>
+                  <FontAwesomeIcon icon={faStar} width="20px"/>
+              </Flex>
+            </Heading>
+            <Text>
+              I am a full stack engineer. I like to build things since I was a little kid.
+            </Text>
+          </VStack>
+          <VStack align="start" spacing={2} marginTop="25px">
             <Heading as="h2" size="md" marginTop="10px" fontFamily="M PLUS Rounded 1c">
               Readme
             </Heading>
             <Text>I am a full stack engineer. I like to build things since I was a little kid.I am a full stack engineer. I like to build things since I was a little kid.I am a full stack engineer. I like to build things since I was a little kid.I am a full stack engineer. I like to build things since I was a little kid.I am a full stack engineer. I like to build things since I was a little kid.</Text>
           </VStack>
-          <Flex justifyContent="center">
+          <Flex justifyContent="center" marginTop="20px">
             <Button backgroundColor="#F4B942" color="#ffffff">Portfolio</Button>
           </Flex>
-          <Heading as="h2" size="md" marginTop="10px" fontFamily="M PLUS Rounded 1c">
-            Works
-          </Heading>
+          <VStack align="start" marginBottom="16px">
+            <Heading as="h2" size="md" marginTop="10px" fontFamily="M PLUS Rounded 1c">
+              Works
+            </Heading>
+          </VStack>
+          <Flex flexWrap="wrap" justifyContent="space-around">
+              <Image  src="/assets/images/computer-icon3.png" boxSize="250px" borderRadius="54px"/>
+              <Image  src="/assets/images/computer-icon5.png" boxSize="250px" borderRadius="54px"/>
+          </Flex>
         </Box>
         <Flex justifyContent="center">
-          <Button backgroundColor="#67C46A" color="#ffffff">Portfolio</Button>
+          <Button backgroundColor="#67C46A" color="#ffffff" marginTop="20px">Portfolio</Button>
         </Flex>
       </Box>
     </div>
