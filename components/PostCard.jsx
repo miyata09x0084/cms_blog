f7a760f5d5b2346de4e334db737f475e8de3f7ff
miyata09x0084 <59190800+miyata09x0084@users.noreply.github.com>
2023-03-20 18:28:40 +0900

    Rewriting elements using chakraui

--- components/PostCard.jsx
+++ components/PostCard.jsx
@@ -2,22 +2,23 @@ import React from 'react';
 import moment from 'moment';
 import Link from 'next/link';
 import { useTheme } from 'styled-components';
+import { Box, Heading } from '@chakra-ui/react';
 
 const PostCard = ({ post }) => {
   const theme = useTheme()
   return (
-    <div className='pt-3 pb-3'>
-      <div className='mb-0.5'>
+    <Box className='pt-3 pb-3'>
+      <Box className='mb-0.5'>
         <span style = {{color: theme.day}}>
           {moment(post.createdAt).format('MMM DD YYYY')}
         </span>
-      </div>
-      <h1 className='text-2xl text-myblue'>
+      </Box>
+      <Heading size="md">
         <Link style = {{color: theme.title}} href={`/post/${post.slug}`}>
           {post.title}
         </Link>
-      </h1>
-    </div>
+      </Heading>
+    </Box>
   )
 }
 
