59ed39268da19fb90b17a24525cc524337dbd745
miyata09x0084 <59190800+miyata09x0084@users.noreply.github.com>
2023-03-20 18:28:49 +0900

    Rewriting elements using chakraui

--- components/Categories.jsx
+++ components/Categories.jsx
@@ -1,24 +1,26 @@
 import React, { useEffect, useState } from 'react';
 import Link from 'next/link';
 import { getCategories } from '../services';
+import { Box, Flex } from '@chakra-ui/react';
 
 const Categories = () => {
   const [categories, setCategories] = useState([]);
+
 
   useEffect(() => {
     getCategories()
       .then((newCategories) => setCategories(newCategories))
   }, [])
   return (
-    <div>
+    <Flex justifyContent="center">
       {categories.map((category) => (
-        <Link key={category.slug} href={`/category/${category.slug}`}>
-          <span className='px-5'>
-            {category.name}
-          </span>
-        </Link>
+        <Box key={category.slug} color="var(--primary-text)" mx="10px">
+          <Link href={`/category/${category.slug}`} >
+              {category.name}
+          </Link>
+        </Box>
       ))}
-    </div>
+    </Flex>
   )
 }
 
