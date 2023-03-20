4fbb74e72b2697ca1427fa007118fcf2e376bd8b
miyata09x0084 <59190800+miyata09x0084@users.noreply.github.com>
2023-03-20 18:31:03 +0900

    Create post.js

---
+++ pages/post.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import { getPosts } from '../services'
+import { Categories, PostCard } from '../components'
+
+const Post = ({posts}) => {
+  return (
+    <div className="mx-auto max-w-screen-md @screen px-8 md:px-0 mb-8 font-MplusRounded">
+        <div className='text-center pt-8 pb-8 border-b border-t border-gray-300 mt-8 mb-12'>
+            <Categories />
+        </div>
+        <div>
+            {posts.map((post) => (<PostCard post={post.node} key={post.title} />))}
+        </div>
+    </div>
+  )
+}
+
+export async function getStaticProps() {
+  const posts = (await getPosts()) || [];
+
+  return {
+    props: { posts }
+  }
+}
+
+export default Post
