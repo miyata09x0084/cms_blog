485f8207ebc19edc17ff44d96d7dfd24956fe906
miyata09x0084 <59190800+miyata09x0084@users.noreply.github.com>
2023-03-23 15:59:02 +0900

    delete google font

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,7 +1,7 @@
 import React, { useContext, useState } from 'react';
 import { useTheme } from 'styled-components';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faDove, faBars } from '@fortawesome/free-solid-svg-icons';
+import { faDove } from '@fortawesome/free-solid-svg-icons';
 import { Box, Flex, Text, Spacer, Link, HStack, IconButton, Drawer, DrawerOverlay, DrawerContent, DrawerCloseButton, DrawerHeader, DrawerBody, VStack, useDisclosure} from '@chakra-ui/react';
 import { HamburgerIcon } from '@chakra-ui/icons';
 
@@ -21,9 +21,11 @@ const Header = () => {
 
   return (
     <header>
-      <Box className="font-MplusRounded" w="100%" mt="10px" mb="30px" fontSize="xl" px={{base: 8, md: 0}} maxWidth="768px" mx="auto" style={{color: theme.font}}>
+      <Box maxWidth="768px" mt="10px" mb="30px" fontSize="xl" px={{base: 8, md: 0}}  mx="auto" style={{color: theme.font}}>
         <Flex>
-            <FontAwesomeIcon icon={faDove} width="20px"/>
+            <Flex alignItems="center" w="20px">
+              <FontAwesomeIcon icon={faDove}/>
+            </Flex>
             <Text mx={1}  fontWeight="bold" display="flex" alignItems="center">
               <Link href="/">Rio Miyata</Link>
             </Text>
@@ -41,7 +43,7 @@ const Header = () => {
         </Flex>
         <Drawer isOpen={isOpen} placement="right" onClose={onClose}>
           <DrawerOverlay>
-            <DrawerContent color="var(--primary-text)" className="font-MplusRounded">
+            <DrawerContent color="var(--primary-text)">
               <DrawerCloseButton/>
               <DrawerHeader borderBottomWidth="1px"  bg="#fcf9f4">Menu</DrawerHeader>
               <DrawerBody  fontWeight="medium" bg="#fcf9f4">
