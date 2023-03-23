3f6d9eb26c3955c2088b8a6cd9b1da68ab11a9bf
miyata09x0084 <59190800+miyata09x0084@users.noreply.github.com>
2023-03-23 15:59:20 +0900

    add animation componets

--- components/index.js
+++ components/index.js
@@ -4,3 +4,6 @@ export { default as Layout } from './Layout';
 export { default as Categories } from './Categories';
 export { default as Header } from './Header';
 export { default as Loader } from './Loader';
+export { default as PixelArt } from './PixelArt'
+export { default as ZoomInFocusImage } from './ZoomInFocusImage'
+export { default as WavingImage } from './WavingImage'
