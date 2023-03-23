bcaff6a1f0a77bbcde8e8f1b6830305db4f8f339
miyata09x0084 <59190800+miyata09x0084@users.noreply.github.com>
2023-03-23 16:00:54 +0900

    make it responsive

--- components/Categories.jsx
+++ components/Categories.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import Link from 'next/link';
 import { getCategories } from '../services';
-import { Box, Flex } from '@chakra-ui/react';
+import { Box, Flex, HStack } from '@chakra-ui/react';
 
 const Categories = () => {
   const [categories, setCategories] = useState([]);
@@ -11,8 +11,9 @@ const Categories = () => {
     getCategories()
       .then((newCategories) => setCategories(newCategories))
   }, [])
+
   return (
-    <Flex justifyContent="center">
+    <Flex justifyContent="center" alignItems="center" width="100%" py="40px" borderTop="1px solid lightgray" borderBottom="1px solid lightgray" mb="30px">
       {categories.map((category) => (
         <Box key={category.slug} color="var(--primary-text)" mx="10px">
           <Link href={`/category/${category.slug}`} >
