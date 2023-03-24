bb4b48db5b710aa12fa64c88f0f995156981125e
miyata09x0084 <59190800+miyata09x0084@users.noreply.github.com>
2023-03-24 16:11:34 +0900

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,10 +4,9 @@ import type { AppProps } from 'next/app';
 import '../styles/global.css';
 import { ThemeProvider } from 'styled-components';
 import { StyledColor, lightTheme, darkTheme } from '../components/ColorTheme';
-import ToggleSwitch from '../components/ToggleSwitch';
-import { ChakraProvider } from "@chakra-ui/react";
+// import ToggleSwitch from '../components/ToggleSwitch';
+import { ChakraProvider, extendTheme } from "@chakra-ui/react";
 import Head from 'next/head';
-import { extendTheme } from "@chakra-ui/react";
 
 function MyApp({ Component, pageProps }: AppProps) {
   const [isDarkTheme, setIsDarkTheme] = useState(false);
@@ -30,13 +29,13 @@ function MyApp({ Component, pageProps }: AppProps) {
         <link href="https://fonts.googleapis.com/css2?family=M+PLUS+Rounded+1c:wght@100;300;400;500;700;800;900&family=Source+Code+Pro:ital,wght@0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet" />
       </Head>
       <ChakraProvider theme={googleFont}>
-          <ThemeProvider theme={isDarkTheme ? darkTheme : lightTheme} >
+          {/* <ThemeProvider theme={isDarkTheme ? darkTheme : lightTheme} > */}
             <StyledColor>
               <Layout>
                 <Component {...pageProps} />
               </Layout>
             </StyledColor>
-          </ThemeProvider>
+          {/* </ThemeProvider> */}
       </ChakraProvider>
     </>
   )
