163460a834dfa45c1f88680a6f7f7432a04a2174
miyata09x0084 <59190800+miyata09x0084@users.noreply.github.com>
2023-03-24 16:13:05 +0900

    Update PostCard.jsx

--- components/PostCard.jsx
+++ components/PostCard.jsx
@@ -1,18 +1,21 @@
 import React from 'react';
 import moment from 'moment';
 import Link from 'next/link';
-import { useTheme } from 'styled-components';
-import { Box, Heading, Text, VStack } from '@chakra-ui/react';
+import { Heading, Text, VStack, useColorMode, useColorModeValue } from '@chakra-ui/react';
 
 const PostCard = ({ post }) => {
-  const theme = useTheme()
+  const { colorMode } = useColorMode();
+  const bg = useColorModeValue("var(--primary-bg)", "var(--dark-bg)");
+  const color = useColorModeValue("var(--primary-text)", "var(--dark-text)");
+  const colorSub = useColorModeValue("var(--secondary-text)", "var(--dark-bg)");
+
   return (
-    <VStack align="start" pb="20px">
-      <Text style = {{color: theme.day}} borderBottom="1px solid lightgray" pb="6px">
+    <VStack align="start" pb="20px" color={color}>
+      <Text borderBottom="1px solid lightgray" pb="6px">
         {moment(post.createdAt).format('MMM DD YYYY')}
       </Text>
       <Heading as="h1" size="md">
-        <Link style = {{color: theme.title}} href={`/post/${post.slug}`}>
+        <Link href={`/post/${post.slug}`}>
           {post.title}
         </Link>
       </Heading>
