63b17c3478ca31bd472ff1c839b5dd5f16c5b161
miyata09x0084 <59190800+miyata09x0084@users.noreply.github.com>
2023-03-24 16:13:18 +0900

    Update Categories.jsx

--- components/Categories.jsx
+++ components/Categories.jsx
@@ -1,21 +1,25 @@
 import React, { useEffect, useState } from 'react';
 import Link from 'next/link';
 import { getCategories } from '../services';
-import { Box, Flex, HStack } from '@chakra-ui/react';
+import { Box, Flex, useColorMode, useColorModeValue } from '@chakra-ui/react';
 
 const Categories = () => {
   const [categories, setCategories] = useState([]);
-
 
   useEffect(() => {
     getCategories()
       .then((newCategories) => setCategories(newCategories))
   }, [])
 
+  const { colorMode } = useColorMode();
+  const bg = useColorModeValue("var(--primary-bg)", "var(--dark-bg)");
+  const color = useColorModeValue("var(--primary-text)", "var(--dark-text)");
+  const colorSub = useColorModeValue("var(--secondary-text)", "var(--dark-bg)");
+
   return (
-    <Flex justifyContent="center" alignItems="center" width="100%" py="40px" borderTop="1px solid lightgray" borderBottom="1px solid lightgray" mb="30px">
+    <Flex justifyContent="center" alignItems="center" width="100%" mt="40px" py="40px" borderTop="1px solid lightgray" borderBottom="1px solid lightgray" mb="30px" color={color}>
       {categories.map((category) => (
-        <Box key={category.slug} color="var(--primary-text)" mx="10px">
+        <Box key={category.slug} mx="10px">
           <Link href={`/category/${category.slug}`} >
               {category.name}
           </Link>
