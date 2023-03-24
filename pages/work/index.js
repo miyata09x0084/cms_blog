a2f64bfc239925a942ad6b11455e5ea0ce2d5222
miyata09x0084 <59190800+miyata09x0084@users.noreply.github.com>
2023-03-24 16:12:00 +0900

    Update index.js

--- pages/work/index.js
+++ pages/work/index.js
@@ -1,9 +1,15 @@
 import React from 'react'
-import { Box, VStack } from '@chakra-ui/react'
+import { Box, VStack, useColorMode, useColorModeValue } from '@chakra-ui/react'
 
 const WorkIndex = () => {
+
+ const { colorMode } = useColorMode();
+ const bg = useColorModeValue("var(--primary-bg)", "var(--dark-bg)");
+ const color = useColorModeValue("var(--primary-text)", "var(--dark-text)");
+ const colorSub = useColorModeValue("var(--secondary-text)", "var(--dark-bg)");
+
   return (
-    <Box align="start" mx="auto" maxW="768px" px={{ base: "8", md: "0" }} mb="8" className="M PLUS Rounded 1c" color="var(--primary-text)">
+    <Box align="start" mx="auto" maxW="768px" h="100vh" px={{ base: "8", md: "0" }} mb="8" className="M PLUS Rounded 1c" color={color} bg={bg} pt="40px">
         現在作成中。
     </Box>
   )
