b58aae9d40d965774d6e855b4b687dc710dd5014
miyata09x0084 <59190800+miyata09x0084@users.noreply.github.com>
2023-03-24 16:11:22 +0900

    Update Header.jsx

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,9 +1,11 @@
-import React, { useContext, useState } from 'react';
+import React from 'react';
 import { useTheme } from 'styled-components';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faDove } from '@fortawesome/free-solid-svg-icons';
-import { Box, Flex, Text, Spacer, Link, HStack, IconButton, Drawer, DrawerOverlay, DrawerContent, DrawerCloseButton, DrawerHeader, DrawerBody, VStack, useDisclosure} from '@chakra-ui/react';
+import { faDove, faSun, faMoon } from '@fortawesome/free-solid-svg-icons';
+import { Box, Flex, Text, Spacer, Link, HStack, IconButton, Drawer, DrawerOverlay, DrawerContent, DrawerCloseButton, DrawerHeader, DrawerBody, VStack, useDisclosure, Button } from '@chakra-ui/react';
 import { HamburgerIcon } from '@chakra-ui/icons';
+import { useColorMode, useColorModeValue } from "@chakra-ui/react"
+import { motion } from 'framer-motion';
 
 
 const Header = () => {
@@ -12,49 +14,85 @@ const Header = () => {
   const { isOpen, onOpen, onClose } = useDisclosure();
 
   const MenuItems = () => (
-    <>
-      <Link href="/post" _hover={{ textDecoration: 'none' }} prefetch>Post</Link>
-      <Link href="/work" _hover={{ textDecoration: 'none' }} prefetch>Work</Link>
-      <Link href="https://github.com/miyata09x0084" _hover={{ textDecoration: 'none' }} prefetch>GitHub</Link>
-    </>
+    <Flex fontWeight="medium">
+      <Link href="/post" _hover={{ textDecoration: 'none' }} >Post</Link>
+      <Link href="/work" _hover={{ textDecoration: 'none' }} ml="12px">Work</Link>
+      <Link href="https://github.com/miyata09x0084" _hover={{ textDecoration: 'none' }} ml="12px">GitHub</Link>
+    </Flex>
   );
+
+  const { colorMode, toggleColorMode } = useColorMode();
+  const bg = useColorModeValue("var(--primary-bg)", "var(--dark-bg)");
+  const color = useColorModeValue("var(--primary-text)", "var(--dark-text)");
+  const colorSub = useColorModeValue("var(--secondary-text)", "var(--dark-bg)");
+
+  const sunIcon = <FontAwesomeIcon icon={faSun} />
+  const moonIcon = <FontAwesomeIcon icon={faMoon} />
+
+  const sunToMoonVariants = {
+    light: { rotate: 0, color: "var(--primary-text)" },
+    dark: { rotate: 180, color: "var(--dark-text)" },
+  };
+
+  const moonToSunVariants = {
+    light: { rotate: 0, color: "var(--primary-text)" },
+    dark: { rotate: -180, color: "var(--dark-text)" },
+  };
 
   return (
     <header>
-      <Box maxWidth="768px" mt="10px" mb="30px" fontSize="xl" px={{base: 8, md: 0}}  mx="auto" style={{color: theme.font}}>
-        <Flex>
-            <Flex alignItems="center" w="20px">
-              <FontAwesomeIcon icon={faDove}/>
+      <Box w="100vw" bg={bg}>
+        <Box maxWidth="768px" pt="10px" fontSize="xl" px={{base: 8, md: 0}}  mx="auto" color={color} bg={bg}>
+            <Flex justifyContent="right" py="10px">
+                  <motion.div
+                    animate={colorMode === 'light' ? 'dark' : 'light'}
+                    variants={colorMode === 'light' ? sunToMoonVariants : moonToSunVariants}
+                    transition={{ duration: 0.6 }}
+                  >
+                    <IconButton
+                      onClick={toggleColorMode}
+                      icon={colorMode === "light" ? sunIcon : moonIcon}
+                      aria-label="Toggle dark mode"
+                      color={color}
+                      _hover={{ bg: {bg} }}
+                      bg={bg}
+                    />
+                  </motion.div>
             </Flex>
-            <Text mx={1}  fontWeight="bold" display="flex" alignItems="center">
-              <Link href="/" _hover={{ textDecoration: 'none' }}>Rio Miyata</Link>
-            </Text>
-            <Spacer />
-            <HStack spacing={4} display={{base: "none", md: "flex"}}>
-              <MenuItems />
-            </HStack>
-            <IconButton
-              display={{base: "flex", md: "none"}}
-              aria-label="Open menu"
-              bg="#524C44"
-              _hover ={{ bg: '#524C44' }}
-              icon ={<HamburgerIcon color="#fcf9f4"/>}
-              onClick={onOpen}
-            />
-        </Flex>
-        <Drawer isOpen={isOpen} placement="right" onClose={onClose}>
-          <DrawerOverlay>
-            <DrawerContent color="var(--primary-text)">
-              <DrawerCloseButton/>
-              <DrawerHeader borderBottomWidth="1px" bg="#fcf9f4">Menu</DrawerHeader>
-              <DrawerBody  fontWeight="medium" bg="#fcf9f4">
-                <VStack align="start">
-                  <MenuItems />
-                </VStack>
-              </DrawerBody>
-            </DrawerContent>
-          </DrawerOverlay>
-        </Drawer>
+          <Flex>
+              <Flex alignItems="center" w="20px">
+                <FontAwesomeIcon icon={faDove}/>
+              </Flex>
+              <Text mx={1} letterSpacing="-1px"  fontWeight="bold" display="flex" alignItems="center">
+                <Link href="/" _hover={{ textDecoration: 'none' }}>Rio Miyata</Link>
+              </Text>
+              <Spacer />
+              <HStack spacing={4} display={{base: "none", md: "flex"}}>
+                <MenuItems />
+              </HStack>
+              <IconButton
+                display={{base: "flex", md: "none"}}
+                aria-label="Open menu"
+                bg={color}
+                _hover ={{ bg: {color} }}
+                icon ={<HamburgerIcon color={colorSub}/>}
+                onClick={onOpen}
+              />
+          </Flex>
+          <Drawer isOpen={isOpen} placement="right" onClose={onClose}>
+            <DrawerOverlay>
+              <DrawerContent color={color}>
+                <DrawerCloseButton/>
+                <DrawerHeader borderBottomWidth="1px" bg={bg}>Menu</DrawerHeader>
+                <DrawerBody  fontWeight="medium" bg={bg}>
+                  <VStack align="start">
+                    <MenuItems />
+                  </VStack>
+                </DrawerBody>
+              </DrawerContent>
+            </DrawerOverlay>
+          </Drawer>
+        </Box>
       </Box>
     </header>
   )
