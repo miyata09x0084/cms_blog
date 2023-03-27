51c0b1dd8b021678dfdae235553da10463806f0c
miyata09x0084 <59190800+miyata09x0084@users.noreply.github.com>
2023-03-27 11:55:40 +0900

    avoid flash

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,8 +7,9 @@ import { StyledColor, lightTheme, darkTheme } from '../components/ColorTheme';
 // import ToggleSwitch from '../components/ToggleSwitch';
 import { ChakraProvider, extendTheme, useColorModeValue } from "@chakra-ui/react";
 import Head from 'next/head';
+import { Chakra } from "../components/Chakra";
 
-function MyApp({ Component, pageProps }: AppProps) {
+function MyApp({ Component, pageProps, cookies }: AppProps & { cookies: string }) {
   const [isDarkTheme, setIsDarkTheme] = useState(false);
   const toggleTheme = () => {
     setIsDarkTheme(!isDarkTheme);
@@ -34,17 +35,19 @@ function MyApp({ Component, pageProps }: AppProps) {
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
         <link href="https://fonts.googleapis.com/css2?family=M+PLUS+Rounded+1c:wght@100;300;400;500;700;800;900&family=Source+Code+Pro:ital,wght@0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet" />
       </Head>
-      <ChakraProvider theme={googleFont}>
-          {/* <ThemeProvider theme={isDarkTheme ? darkTheme : lightTheme} > */}
-            {/* <StyledColor> */}
-              <Layout>
-                <Component {...pageProps} />
-              </Layout>
-            {/* </StyledColor> */}
-          {/* </ThemeProvider> */}
-      </ChakraProvider>
+        <Chakra cookies={cookies} >
+            {/* <ThemeProvider theme={isDarkTheme ? darkTheme : lightTheme} > */}
+              {/* <StyledColor> */}
+                <Layout>
+                  <Component {...pageProps} />
+                </Layout>
+              {/* </StyledColor> */}
+            {/* </ThemeProvider> */}
+        </Chakra>
     </>
   )
 }
 
 export default MyApp
+
+export { getServerSideProps } from '../components/Chakra';
