8e16676de33eae6ffb92466c8efa8961b070f94f
miyata09x0084 <59190800+miyata09x0084@users.noreply.github.com>
2023-03-27 11:57:01 +0900

    avoid flash

---
+++ components/Chakra.jsx
@@ -0,0 +1,22 @@
+import { ChakraProvider, cookieStorageManagerSSR, localStorageManager } from "@chakra-ui/react";
+
+export function Chakra({ cookies, children}) {
+    const colorModeManager =
+        typeof cookies === "string"
+            ? cookieStorageManagerSSR(cookies)
+            : localStorageManager
+
+    return (
+        <ChakraProvider colorModeManager={colorModeManager}>
+            {children}
+        </ChakraProvider>
+    )
+}
+
+export function getServerSideProps({ req }) {
+    return {
+        props: {
+            cookies: req.headers.cookie ?? "",
+        }
+    }
+}
