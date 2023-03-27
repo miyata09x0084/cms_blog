5cdc1e53d4625a8e2d2862659407ff86156e0a9d
miyata09x0084 <59190800+miyata09x0084@users.noreply.github.com>
2023-03-27 13:45:14 +0900

    refactoring

--- components/PostCard.jsx
+++ components/PostCard.jsx
@@ -1,16 +1,12 @@
 import React from 'react';
 import moment from 'moment';
 import Link from 'next/link';
-import { Heading, Text, VStack, useColorMode, useColorModeValue } from '@chakra-ui/react';
+import { Heading, Text, VStack } from '@chakra-ui/react';
 
 const PostCard = ({ post }) => {
-  const { colorMode } = useColorMode();
-  const bg = useColorModeValue("var(--primary-bg)", "var(--dark-bg)");
-  const color = useColorModeValue("var(--primary-text)", "var(--dark-text)");
-  const colorSub = useColorModeValue("var(--secondary-text)", "var(--dark-bg)");
 
   return (
-    <VStack align="start" pb="20px" color={color}>
+    <VStack align="start" pb="20px">
       <Text borderBottom="1px solid lightgray" pb="6px">
         {moment(post.createdAt).format('MMM DD YYYY')}
       </Text>
