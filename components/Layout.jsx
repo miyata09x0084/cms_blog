1862697ea13a46f24a7e7ee482e8da612612e76b
miyata09x0084 <59190800+miyata09x0084@users.noreply.github.com>
2023-03-27 11:57:37 +0900

    faster import

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,13 +1,18 @@
 import React from 'react';
 import { Header } from './';
+import { useColorModeValue } from '@chakra-ui/react';
+import { Box } from '@chakra-ui/react';
 
 const Layout = ({ children }) => {
 
+  const bg = useColorModeValue("var(--primary-bg)", "var(--dark-bg)");
+  const color = useColorModeValue("var(--primary-text)", "var(--dark-text)");
+
   return (
-    <>
+    <Box bg={bg} color={color}>
        <Header />
        { children }
-    </>
+    </Box>
   )
 }
 
