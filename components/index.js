b29b4e29fd4eb9d3eb74fc2c2ebf0c752b64175e
miyata09x0084 <59190800+miyata09x0084@users.noreply.github.com>
2023-03-29 20:54:16 +0900

    add footer

--- components/index.js
+++ components/index.js
@@ -6,3 +6,4 @@ export { default as Header } from './Header';
 export { default as Loader } from './Loader';
 export { default as ZoomInFocusImage } from './ZoomInFocusImage'
 export { default as WavingImage } from './WavingImage'
+export { default as Footer } from './Footer'
