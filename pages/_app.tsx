130c8180a97dfe1f0ca78c1fb06b7f095132e0b3
miyata09x0084 <59190800+miyata09x0084@users.noreply.github.com>
2023-03-30 16:13:29 +0900

    clean

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,6 @@
 import { Layout } from '../components';
 import type { AppProps } from 'next/app';
 import '../styles/global.css';
-// import { ThemeProvider } from 'styled-components';
-// import { StyledColor, lightTheme, darkTheme } from '../components/ColorTheme';
-// import ToggleSwitch from '../components/ToggleSwitch';
 import { Chakra } from "../components/Chakra";
 
 function MyApp({ Component, pageProps }: AppProps) {
