2d8fc0099c36872f82d2b9362631b132e32e8f67
miyata09x0084 <59190800+miyata09x0084@users.noreply.github.com>
2023-05-22 15:45:39 +0900

    change font

--- pages/_document.js
+++ pages/_document.js
@@ -9,7 +9,7 @@ export default class Document extends NextDocument {
                 <Head>
                     <link rel="preconnect" href="https://fonts.googleapis.com" />
                     <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
-                    <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@400;500;600;700;800;900&family=Rampart+One&family=Source+Code+Pro:ital,wght@0,500;0,600;0,700;0,800;0,900;1,500;1,600&display=swap" rel="stylesheet" />
+                    <link href="https://fonts.googleapis.com/css2?family=M+PLUS+1p:wght@400;500;700;800;900&family=Noto+Sans+JP:wght@400;500;600;700;800;900&family=Source+Code+Pro:ital,wght@0,500;0,600;0,700;0,800;0,900;1,500;1,600&display=swap" rel="stylesheet" />
                 </Head>
                 <body>
                     <ColorModeScript initialColorMode={theme.config.initialColorMode} />
