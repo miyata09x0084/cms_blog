5d4dc096d2dce38063922ae549299d458548189f
miyata09x0084 <59190800+miyata09x0084@users.noreply.github.com>
2023-06-07 12:56:01 +0900

    change name

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -43,7 +43,7 @@ const Footer = () => {
         </Box>
       </Box>
       <Box fontSize="sm" px={{base: 4, md: 0}} mx="auto" mb={6} textAlign="center" color={color}>
-        <Text>© 2023 Meercaton. All rights reserved.</Text>
+        <Text>© 2023 Rio Miyata. All rights reserved.</Text>
         <Text>Thanks for reading!</Text>
       </Box>
     </footer>
