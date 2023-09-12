d7c5ac34923e17277d9b74a4b6ff808e72c052ad
miyata09x0084 <59190800+miyata09x0084@users.noreply.github.com>
2023-09-12 12:46:13 +0900

    Update TypingAnimation.jsx

--- components/TypingAnimation.jsx
+++ components/TypingAnimation.jsx
@@ -1,23 +1,10 @@
 import { Box, Flex } from "@chakra-ui/react";
-import { motion } from "framer-motion";
-
-const sentence = "Hello, I'm Full Stack Developer based in Japan.";
-const duration = 0.15; // 任意のタイムデュレーションを設定
 
 const TypingAnimation = () => {
   return (
-    <Flex w="100%" justifyContent="center" alignItems="center" fontFamily="Noto Sans JP">
-        <Box  mx={{base: 2, md: 0}}  px={4} fontWeight="900" pt={1.5} pb={2} mt={1} borderRadius={14} textAlign="center" bg="#664D03" color="#EDDFD6">
-          {sentence.split("").map((char, index) => (
-            <motion.span
-              key={index}
-              initial={{ opacity: 0 }}
-              animate={{ opacity: 1 }}
-              transition={{ delay: index * duration }}
-            >
-              {char}
-            </motion.span>
-          ))}
+    <Flex w="100%" justifyContent="center" alignItems="center" pt={1} fontFamily="Noto Sans JP" >
+        <Box mx={{base: 2, md: 0}}  px={3} fontWeight="900"  pt={0.5} pb={2} borderRadius={12} textAlign="center" bg="#664D03" color="#EDDFD6">
+            <Box className='typing-animation' px={1.5}>Hello, I'm Full Stack Developer based in Japan.</Box>
         </Box>
     </Flex>
   );
