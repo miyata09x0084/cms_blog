70f3f2a10f4ea29ed8cd174281a3d27f82e0d322
miyata09x0084 <59190800+miyata09x0084@users.noreply.github.com>
2023-09-12 12:26:29 +0900

    横幅を狭くする

--- components/TypingAnimation.jsx
+++ components/TypingAnimation.jsx
@@ -1,4 +1,4 @@
-import { Box } from "@chakra-ui/react";
+import { Box, Flex } from "@chakra-ui/react";
 import { motion } from "framer-motion";
 
 const sentence = "Hello, I'm Full Stack Developer based in Japan.";
@@ -6,18 +6,20 @@ const duration = 0.15; // 任意のタイムデュレーションを設定
 
 const TypingAnimation = () => {
   return (
-    <Box fontFamily="Noto Sans JP"  mx={{base: 2, md: 0}}  px={3} fontWeight="600"  pt={1} pb={2} mt={10} borderRadius={12} textAlign="center" bg="#664D03" color="#EDDFD6">
-      {sentence.split("").map((char, index) => (
-        <motion.span
-          key={index}
-          initial={{ opacity: 0 }}
-          animate={{ opacity: 1 }}
-          transition={{ delay: index * duration }}
-        >
-          {char}
-        </motion.span>
-      ))}
-    </Box>
+    <Flex w="100%" justifyContent="center" alignItems="center" fontFamily="Noto Sans JP">
+        <Box  mx={{base: 2, md: 0}}  px={4} fontWeight="900" pt={1.5} pb={2} mt={1} borderRadius={14} textAlign="center" bg="#664D03" color="#EDDFD6">
+          {sentence.split("").map((char, index) => (
+            <motion.span
+              key={index}
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              transition={{ delay: index * duration }}
+            >
+              {char}
+            </motion.span>
+          ))}
+        </Box>
+    </Flex>
   );
 };
 
