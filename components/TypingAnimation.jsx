1c46915b64a173083534a17ec715574b8b900fce
miyata09x0084 <59190800+miyata09x0084@users.noreply.github.com>
2023-09-17 09:31:04 +0900

    Update TypingAnimation.jsx

--- components/TypingAnimation.jsx
+++ components/TypingAnimation.jsx
@@ -3,7 +3,7 @@ import { Box, Flex } from "@chakra-ui/react";
 const TypingAnimation = () => {
   return (
     <Flex w="100%" justifyContent="center" alignItems="center" pt={1} fontFamily="Noto Sans JP" >
-        <Box mx={{base: 2, md: 0}}  px={3} fontWeight="900"  pt={0.5} pb={2} borderRadius={30} textAlign="center" bg="#664D03" color="#EDDFD6">
+        <Box mx={{base: 2, md: 0}}  px={3} fontWeight="800"  pt={0.5} pb={2} borderRadius={30} textAlign="center" bg="#664D03" color="#EDDFD6">
             <Box className='typing-animation' px={1.5}>Hello, I'm Full Stack Developer based in Japan.</Box>
         </Box>
     </Flex>
