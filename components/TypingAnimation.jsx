48c133ef92b79c8918d8cff1f3607bd6f76d76aa
miyata09x0084 <59190800+miyata09x0084@users.noreply.github.com>
2023-09-17 13:41:32 +0900

    視認性を上げる

--- components/TypingAnimation.jsx
+++ components/TypingAnimation.jsx
@@ -4,7 +4,7 @@ const TypingAnimation = () => {
   return (
     <Flex w="100%" justifyContent="center" alignItems="center" pt={3} fontFamily="Noto Sans JP" >
         <Box mx={{base: 2, md: 0}}  px={3} fontWeight="800"  pt={0.5} pb={2} borderRadius={30} textAlign="center" bg="#664D03" color="#EDDFD6">
-            <Box className='typing-animation' px={1.5}>Hello, I'm Full Stack Developer based in Japan.</Box>
+            <Box className='typing-animation' px={1.5}>Hello, I'm Full Stack Developer based in Japan</Box>
         </Box>
     </Flex>
   );
