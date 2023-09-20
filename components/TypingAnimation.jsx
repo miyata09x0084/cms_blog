8c62a3bf882037b3247338fef0180ae460d5d0c9
miyata09x0084 <59190800+miyata09x0084@users.noreply.github.com>
2023-09-20 12:38:15 +0900

    Update TypingAnimation.jsx

--- components/TypingAnimation.jsx
+++ components/TypingAnimation.jsx
@@ -9,7 +9,7 @@ const TypingAnimation = () => {
     const typed = new Typed(el.current, {
       strings: [
         "Hello, ^200 I'm Full Stack Developer ^200 based in Japan. ^1500" ,
-        "Enjoy ^200 browsing my website! ^1500",
+        "Enjoy ^300 browsing my website! ^1500",
       ],
       smartBackspace: true,
       typeSpeed: 50,
