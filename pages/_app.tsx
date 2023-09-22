16b36a55cca02a4dac9ba363f8832389f551c539
miyata09x0084 <59190800+miyata09x0084@users.noreply.github.com>
2023-09-22 11:07:23 +0900

    googleアナリティクスの導入

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,11 +2,42 @@ import { Layout } from '../components';
 import type { AppProps } from 'next/app';
 import '../styles/global.css';
 import { Chakra } from "../components/Chakra";
+import Script from 'next/script';
+import * as gtag from '../lib/gtag';
+import { useRouter } from 'next/router';
+import { useEffect } from 'react';
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+  useEffect(() => {
+    const handleRouteChange = (url: URL) => {
+      gtag.pageview(url);
+    }
+    router.events.on('routeChangeComplete', handleRouteChange);
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange);
+    }
+  }, [router.events]);
 
   return (
     <>
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
+      />
+      <Script
+        id ="gtag-init"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments)}
+            gtag('js', new Date());
+            gtag('config', '${gtag.GA_TRACKING_ID}', {
+              page_path: window.location.pathname,
+            });
+          `,
+        }}
         <Chakra>
           <Layout>
             <Component {...pageProps} />
