45f067219e587ee775413b6db65170086c518cfe
miyata09x0084 <59190800+miyata09x0084@users.noreply.github.com>
2023-09-22 11:12:19 +0900

    タグ閉じ忘れ

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -38,6 +38,7 @@ function MyApp({ Component, pageProps }: AppProps) {
             });
           `,
         }}
+      />
         <Chakra>
           <Layout>
             <Component {...pageProps} />
