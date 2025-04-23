82daf1ea2386f2eaefc8c29b56b4d31ba782bb43
miyata09x0084 <59190800+miyata09x0084@users.noreply.github.com>
2025-04-23 17:28:48 +0900

    プロフィールセクションを更新し、画像を新しいものに差し替えました。また、経歴の内容を整理し、インターフェースを改善しました。

--- pages/index.tsx
+++ pages/index.tsx
@@ -67,216 +67,208 @@ const Home: NextPage<Props> = ({ posts }) => {
             loading="eager"
           />
           <TypingAnimation />
-            <Flex
-              width="100%"
-              justifyContent="center"
-              mt={14}
-              alignItems="center"
-            >
+          <Flex
+            width="100%"
+            justifyContent="center"
+            mt={14}
+            alignItems="center"
+          >
+            <Box>
+              <Text fontSize="4xl" fontWeight="800">
+                Rio Miyata
+              </Text>
+              <Text fontSize="lg" fontWeight="900" mt="-4px">
+                Software Engineer
+              </Text>
+            </Box>
+            <Image
+              src="/assets/images/self-image.jpg"
+              width={72}
+              height={72}
+              priority={true}
+              loading="eager"
+              className="round-image"
+              alt="Rio Miyata"
+            />
+          </Flex>
+          <Box marginLeft="8px" mt={14}>
+            <VStack align="start" spacing={5} mb={12}>
+              <Heading as="h2" fontSize="2xl" fontWeight="900">
+                /Bio
+              </Heading>
+              <VStack align="start" spacing={1.5}>
+                <Flex>
+                  <Text mr="10px" fontWeight="900">
+                    1989
+                  </Text>
+                  <Box>
+                    <Text>Born in Aichi, Japan </Text>
+                    <Text>日本の愛知県出身</Text>
+                  </Box>
+                </Flex>
+                <Flex>
+                  <Text mr="10px" fontWeight="900">
+                    2014
+                  </Text>
+                  <Box>
+                    <Text>
+                      Master's Degree, Graduate School of Science and
+                      Engineering @Hosei University
+                    </Text>
+                    <Text>法政大学理工学研究科 修士課程修了</Text>
+                  </Box>
+                </Flex>
+                <Flex>
+                  <Text mr="10px" fontWeight="900">
+                    2019
+                  </Text>
+                  <Box>
+                    <Text>Server-Side Engineering Course @Tech Camp</Text>
+                    <Text>サーバーサイドエンジニアコース修了</Text>
+                  </Box>
+                </Flex>
+                <Flex>
+                  <Text mr="10px" fontWeight="900">
+                    2020
+                  </Text>
+                  <Box>
+                    <Text>Web Developer @AnkhSystems (Digital Media)</Text>
+                    <Text>アンクシステムズ-デジタルメディア開発</Text>
+                  </Box>
+                </Flex>
+                <Flex>
+                  <VStack spacing={0} align="center" mr="10px">
+                    <Text fontWeight="900">2023</Text>
+                    <Text fontWeight="900" transform="rotate(90deg)" pb="5px">
+                      〜
+                    </Text>
+                  </VStack>
+
+                  <Box>
+                    <Text>Freelance Software Engineer @Japan</Text>
+                    <Text>フリーランスの活動開始</Text>
+                  </Box>
+                </Flex>
+              </VStack>
+            </VStack>
+            <VStack align="start" spacing={5} mb={12}>
+              <Heading as="h2" fontSize="2xl" fontWeight="900">
+                /Interests
+              </Heading>
+              <Text>#Thinking #FoodTouring #Camping #Traveling</Text>
+            </VStack>
+
+            <VStack align="start" spacing={5} mb={12}>
+              <Heading as="h2" fontSize="2xl" fontWeight="900">
+                <HStack alignItems="center">
+                  <Text>/About</Text>
+                </HStack>
+              </Heading>
+              <Text>
+                I am the younger twin brother. I love using programming to
+                manifest my diverse ideas into real-world applications. It's my
+                passion to express these concepts through the creation of apps.
+              </Text>
+            </VStack>
+            <VStack align="start" spacing={5} mb={8}>
+              <Heading as="h2" fontSize="2xl" fontWeight="900">
+                /Creations
+              </Heading>
               <Box>
-                <Text fontSize="4xl" fontWeight="800">
-                  Rio Miyata
-                </Text>
-                <Text fontSize="lg" fontWeight="900" mt="-4px">
-                  Software Engineer
-                </Text>
-              </Box>
-              <Image
-                src="/assets/images/face-image.png"
-                width={72}
-                height={72}
-                priority={true}
-                loading="eager"
-                className="round-image"
-                alt="Rio Miyata"
-              />
-            </Flex>
-            <Box marginLeft="8px" mt={14}>
-              <VStack align="start" spacing={5} mb={12}>
-                <Heading as="h2" fontSize="2xl" fontWeight="900">
-                  /Bio
-                </Heading>
-                <VStack align="start" spacing={1.5}>
-                  <Flex>
-                    <Text mr="10px" fontWeight="900">
-                      1989
-                    </Text>
-                    <Box>
-                      <Text>Born in Aichi, Japan </Text>
-                      <Text>日本の愛知県出身</Text>
-                    </Box>
-                  </Flex>
-                  <Flex>
-                    <Text mr="10px" fontWeight="900">
-                      2014
-                    </Text>
-                    <Box>
-                      <Text>
-                        Master's Degree, Graduate School of Science and
-                        Engineering @Hosei University
-                      </Text>
-                      <Text>法政大学理工学研究科 修士課程修了</Text>
-                    </Box>
-                  </Flex>
-                  <Flex>
-                    <Text mr="10px" fontWeight="900">
-                      2019
-                    </Text>
-                    <Box>
-                      <Text>Server-Side Engineering Course @Tech Camp</Text>
-                      <Text>サーバーサイドエンジニアコース修了</Text>
-                    </Box>
-                  </Flex>
-                  <Flex>
-                    <Text mr="10px" fontWeight="900">
-                      2020
-                    </Text>
-                    <Box>
-                      <Text>Web Developer @AnkhSystems (Digital Media)</Text>
-                      <Text>アンクシステムズ-デジタルメディア開発</Text>
-                    </Box>
-                  </Flex>
-                  <Flex>
-                    <VStack spacing={0} align="center" mr="10px">
-                      <Text fontWeight="900">2023</Text>
-                      <Text fontWeight="900" transform="rotate(90deg)" pb="5px">
-                        〜
-                      </Text>
-                    </VStack>
-
-                    <Box>
-                      <Text>Freelance Software Engineer @Japan</Text>
-                      <Text>フリーランスの活動開始</Text>
-                    </Box>
-                  </Flex>
-                </VStack>
-              </VStack>
-              <VStack align="start" spacing={5} mb={12}>
-                <Heading as="h2" fontSize="2xl" fontWeight="900">
-                  /Interests
-                </Heading>
-                <Text>#Thinking #FoodTouring #Camping #Traveling</Text>
-              </VStack>
-
-              <VStack align="start" spacing={5} mb={12}>
-                <Heading as="h2" fontSize="2xl" fontWeight="900">
-                  <HStack alignItems="center">
-                    <Text>/About</Text>
-                  </HStack>
-                </Heading>
-                <Text>
-                  I am the younger twin brother. I love using programming to
-                  manifest my diverse ideas into real-world applications. It's
-                  my passion to express these concepts through the creation of
-                  apps.
-                </Text>
-              </VStack>
-              <VStack align="start" spacing={5} mb={8}>
-                <Heading as="h2" fontSize="2xl" fontWeight="900">
-                  /Creations
-                </Heading>
-                <Box>
-                  <Flex
-                    justifyContent="space-between"
-                    width="100%"
-                    height="100%"
-                  >
-                    <Box w="280px">
-                      <Link
-                        href="https://kangeki-dapps.web.app/"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                      >
-                        <Image
-                          src="/assets/images/dev-image1.png"
-                          alt="Image 1"
-                          width={280}
-                          height={158}
-                          quality={90}
-                          loading="lazy"
-                          className="round-card"
-                        />
-                      </Link>
-                    </Box>
-                  </Flex>
-                </Box>
-              </VStack>
-              <Flex justifyContent="center" mb={10}>
-                <Link href="/work">
-                  <Button
-                    fontWeight="600"
-                    borderRadius="25px"
-                    bg="#8505b0 !important" // 強制的に背景色を適用
-                    color="#EDDFD6"
-                    boxShadow="2px 2px 10px rgba(133, 5, 176, 0.3)"
-                    _hover={{ opacity: 0.4 }}
-                    pb={1}
-                  >
-                    <Flex alignItems="center" mr={-1}>
-                      <Text mr={0.5}>Creations</Text>
-                      <FontAwesomeIcon
-                        icon={faCaretRight}
-                        width={15}
-                        height={15}
+                <Flex justifyContent="space-between" width="100%" height="100%">
+                  <Box w="280px">
+                    <Link
+                      href="https://kangeki-dapps.web.app/"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      <Image
+                        src="/assets/images/dev-image1.png"
+                        alt="Image 1"
+                        width={280}
+                        height={158}
+                        quality={90}
+                        loading="lazy"
+                        className="round-card"
                       />
-                    </Flex>
-                  </Button>
-                </Link>
-              </Flex>
-              <VStack align="start" spacing={5}>
-                <Heading as="h2" fontSize="2xl" fontWeight="900">
-                  /Posts
-                </Heading>
-                {/* ブログの埋め込み */}
-                {/* <VStack align="start">
+                    </Link>
+                  </Box>
+                </Flex>
+              </Box>
+            </VStack>
+            <Flex justifyContent="center" mb={10}>
+              <Link href="/work">
+                <Button
+                  fontWeight="600"
+                  borderRadius="25px"
+                  bg="#8505b0 !important" // 強制的に背景色を適用
+                  color="#EDDFD6"
+                  boxShadow="2px 2px 10px rgba(133, 5, 176, 0.3)"
+                  _hover={{ opacity: 0.4 }}
+                  pb={1}
+                >
+                  <Flex alignItems="center" mr={-1}>
+                    <Text mr={0.5}>Creations</Text>
+                    <FontAwesomeIcon
+                      icon={faCaretRight}
+                      width={15}
+                      height={15}
+                    />
+                  </Flex>
+                </Button>
+              </Link>
+            </Flex>
+            <VStack align="start" spacing={5}>
+              <Heading as="h2" fontSize="2xl" fontWeight="900">
+                /Posts
+              </Heading>
+              {/* ブログの埋め込み */}
+              {/* <VStack align="start">
                   {posts.map((post, index) => (
                     <PostCard post={post.node} key={index} />
                   ))}
                 </VStack> */}
-                <VStack align="start" mb={5}>
-                  <iframe
-                    className="note-embed"
-                    src="https://note.com/embed/notes/n3e17e24dd31c"
-                    style={{
-                      border: 0,
-                      display: "block",
-                      maxWidth: "99%",
-                      width: "494px",
-                      padding: "0px",
-                      position: "static",
-                      visibility: "visible",
-                    }}
-                    height="400"
-                  ></iframe>
-                  <script
-                    async
-                    src="https://note.com/scripts/embed.js"
-                  ></script>
-                </VStack>
+              <VStack align="start" mb={5}>
+                <iframe
+                  className="note-embed"
+                  src="https://note.com/embed/notes/n3e17e24dd31c"
+                  style={{
+                    border: 0,
+                    display: "block",
+                    maxWidth: "99%",
+                    width: "494px",
+                    padding: "0px",
+                    position: "static",
+                    visibility: "visible",
+                  }}
+                  height="400"
+                ></iframe>
+                <script async src="https://note.com/scripts/embed.js"></script>
               </VStack>
-              <Flex justifyContent="center" mb={12}>
-                <Link href="/post">
-                  <Button
-                    fontWeight="600"
-                    borderRadius="25px"
-                    bg="#0530b0 !important" // 強制的に背景色を適用
-                    color="#EDDFD6"
-                    boxShadow="2px 2px 10px rgba(5, 48, 176, 0.3)"
-                    _hover={{ opacity: 0.4 }}
-                    pb={1}
-                  >
-                    <Flex alignItems="center" mr={-1}>
-                      <Text mr={0.5}>Posts</Text>
-                      <FontAwesomeIcon
-                        icon={faCaretRight}
-                        width={15}
-                        height={15}
-                      />
-                    </Flex>
-                  </Button>
-                </Link>
-              </Flex>
-            </Box>
+            </VStack>
+            <Flex justifyContent="center" mb={12}>
+              <Link href="/post">
+                <Button
+                  fontWeight="600"
+                  borderRadius="25px"
+                  bg="#0530b0 !important" // 強制的に背景色を適用
+                  color="#EDDFD6"
+                  boxShadow="2px 2px 10px rgba(5, 48, 176, 0.3)"
+                  _hover={{ opacity: 0.4 }}
+                  pb={1}
+                >
+                  <Flex alignItems="center" mr={-1}>
+                    <Text mr={0.5}>Posts</Text>
+                    <FontAwesomeIcon
+                      icon={faCaretRight}
+                      width={15}
+                      height={15}
+                    />
+                  </Flex>
+                </Button>
+              </Link>
+            </Flex>
+          </Box>
 
           <VStack align="start" mb={6}>
             <Heading as="h2" fontSize="2xl" fontWeight="900">
